ccf1520428c307c35f154edf38d468bd65ee4ab6
edwin-od <67093951+edwin-od@users.noreply.github.com>
2020-10-17 22:53:50 +0300

    Update edwin.py

--- edwin.py
+++ edwin.py
@@ -34,7 +34,7 @@ class Feature:
         for option in this.options:
             count = 0
             index = 0
-            tempTarget = TargetFeature(this.target.name + "-" + str(option), this.target.options, [])
+            tempTarget = TargetFeature(this.target.name, this.target.options, [])
             for value in this.values:
                 if value == option:
                     count = count + 1
@@ -47,10 +47,79 @@ class Feature:
         return g
 
 
+class TreeNode:
+    def __init__(this, name, parent, parentLink, children):
+        this.name = name
+        this.parent = parent
+        this.parentLink = parentLink
+        this.children = children
+
+    def printTree(this):
+        print(this.name)
+        p = this.parent
+        s = "  ";
+        while p != None:
+            s += "   "
+            p = p.parent
+        if(this.children == None):
+            return
+        for child in this.children:
+            print(s + "|__(" + str(child.parentLink) + ")__", end = "")
+            child.printTree()
+
+
+class DecisionTable:
+    def __init__(this, features, target):
+        this.features = features
+        this.target = target
+
+    def generateDecisionTree(this):
+        #Find the root
+        maxGainFeature = None
+        for feature in this.features:
+            if maxGainFeature == None or feature.G() >= maxGainFeature.G():
+                maxGainFeature = feature
+        tree = TreeNode(maxGainFeature.name, None, None, [])
+
+        #Create new table (and find root + child tables, etc.) for each option of root
+        for option in maxGainFeature.options:
+            count = 0
+            index = 0
+            tempTarget = TargetFeature(this.target.name, this.target.options, [])
+            tempFeatures = []
+            for feature in this.features:
+                if feature.name == maxGainFeature.name:
+                    continue
+                tempFeatures.append(Feature(feature.name, feature.options, [], tempTarget))
+            for value in maxGainFeature.values:
+                if value == option:
+                    count = count + 1
+                    tempTarget.values.append(this.target.values[index])
+                    for tempFeature in tempFeatures:
+                        for feature in this.features:
+                            if feature.name == tempFeature.name:
+                                tempFeature.values.append(feature.values[index])
+                                break
+                index = index + 1
+            if tempTarget.H() == 0:
+                tree.children.append(TreeNode(tempTarget.values[0], tree, option, None))
+                continue
+            tempTable = DecisionTable(tempFeatures, tempTarget)
+            tree.children.append(tempTable.generateDecisionTree())
+            tree.children[len(tree.children) - 1].parent = tree
+            tree.children[len(tree.children) - 1].parentLink = option
+        return tree
+
+
+#Test Example Chapter 7 Exercise 7.3
+
 Accept = TargetFeature("Acceptable", ["Yes", "No"], ["Yes", "No", "Yes", "No", "Yes"])
 
 furn = Feature("Furniture", ["Yes", "No"], ["No", "Yes", "No", "No", "Yes"], Accept)
 rooms = Feature("Nr Rooms", [3, 4], [3, 3, 4, 3, 4], Accept)
 newKit = Feature("New Kitchen", ["Yes", "No"], ["Yes", "No", "No", "No", "No"], Accept)
 
-print(furn.G())
+table = DecisionTable([furn, rooms, newKit], Accept)
+
+tree = table.generateDecisionTree()
+tree.printTree()
