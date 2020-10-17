d202da64f16a4d467cf6ddc0f3e07b07e14a3be4
Hazard <55458430+michelman99@users.noreply.github.com>
2020-10-17 23:45:57 +0300

    black and white

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,8 @@
 import cv2
+import os
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('img1.jpg')
+img = cv2.imread('img4.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #transform to gray
 faces = face_cascade.detectMultiScale(gray,1.1,10)#detects faces in the image and draws rectangle around the faces
 
@@ -9,10 +10,12 @@ for (x,y,w,h) in faces :
     cv2.rectangle(img , (x,y) , (x+w,y+h) , (0,0,255), 4)
 
 imgCropped = img[y+3:y+h-2,x+3:x+w-2]
-#imgCroppedResized = cv2.resize(imgCropped,(img.shape[1],img.shape[0]))
-
+grayImage = cv2.cvtColor(imgCropped, cv2.COLOR_BGR2GRAY)
+(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 100, 255, cv2.THRESH_BINARY)
 #produce the output
 cv2.imshow('face detected', img)
+cv2.imshow('gray', grayImage)
 cv2.imshow('cropped face resized', imgCropped)
+cv2.imshow('Black white image', blackAndWhiteImage)
 cv2.waitKey()
 
