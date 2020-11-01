a2c65f8b0ea54af35f6ff836318c26a7d743e9a2
edwin-od <67093951+edwin-od@users.noreply.github.com>
2020-11-01 20:26:01 +0200

    Update feature_detection.py

--- feature_detection.py
+++ feature_detection.py
@@ -4,59 +4,67 @@ import dlib
 
 from isolated_features import IsolatedFeatures
 
-url = 'input3.jpg'
+#r = 8
 
-img = cv2.imread(url)
-img = cv2.resize(img, (320, 400),  interpolation = cv2.INTER_AREA)
+for r in range(1, 15):
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    url = 'input' + str(r) + '.jpg'
 
-detector = dlib.get_frontal_face_detector()
-predictor =  dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    img = cv2.imread(url)
+    img = cv2.resize(img, (320, 400),  interpolation = cv2.INTER_AREA)
 
-face = detector(gray)[0]
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-landmarks = predictor(gray, face)
+    detector = dlib.get_frontal_face_detector()
+    predictor =  dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-features = IsolatedFeatures(landmarks)
+    face = detector(gray)[0]
 
-poly = features.LeftCheekNoBeard
-facePoly = features.FaceNoBeard
+    landmarks = predictor(gray, face)
+    features = IsolatedFeatures(landmarks)
 
-mask = np.zeros((img.shape[0], img.shape[1]))
-cv2.fillConvexPoly(mask, poly, 1)
-mask = mask.astype(np.bool)
+    #Average Face Brightness
+    fullMask = np.zeros((img.shape[0], img.shape[1]))
+    cv2.fillConvexPoly(fullMask, features.FullCheeks, 1)
+    fullMask = fullMask.astype(np.bool)
+    AVG_pix_gray = np.sum(gray[fullMask]) / np.sum(fullMask == 1)
+    (thresh, blackAndWhiteImage) = cv2.threshold(gray, AVG_pix_gray, 255, cv2.THRESH_BINARY)
 
-faceMask = np.zeros((img.shape[0], img.shape[1]))
-cv2.fillConvexPoly(faceMask, facePoly, 1)
-faceMask = faceMask.astype(np.bool)
+    LeftMask = np.zeros((img.shape[0], img.shape[1]))
+    cv2.fillConvexPoly(LeftMask, features.LeftCheek, 1)
+    LeftMask = LeftMask.astype(np.bool)
+    LeftBlackPix = int((np.sum(blackAndWhiteImage[LeftMask] == 0) / np.sum(LeftMask == 1)) * 100)
 
-SUM_pix_gray = np.sum(gray[faceMask])
-AVG_pix_gray = SUM_pix_gray / np.sum(faceMask == 1)
+    RightMask = np.zeros((img.shape[0], img.shape[1]))
+    cv2.fillConvexPoly(RightMask, features.RightCheek, 1)
+    RightMask = RightMask.astype(np.bool)
+    RightBlackPix = int((np.sum(blackAndWhiteImage[RightMask] == 0) / np.sum(RightMask == 1)) * 100)
 
-(thresh, blackAndWhiteImage) = cv2.threshold(gray, AVG_pix_gray - 15, 255, cv2.THRESH_BINARY)
+    if LeftBlackPix < RightBlackPix:
+        mask = LeftMask
+        poly = features.LeftCheek
+    else:
+        mask = RightMask
+        poly = features.RightCheek
+        
+    threshold = np.sum(gray[mask] - 25) / np.sum(mask == 1)
 
-#get pixels
-white_pix = int((np.sum(blackAndWhiteImage[mask] == 255) / np.sum(mask == 1)) * 100)
-black_pix = int((np.sum(blackAndWhiteImage[mask] == 0) / np.sum(mask == 1)) * 100)
-print('URL:', url)
-print('AVG:', AVG_pix_gray)
-print('white:', white_pix, '%')
-print('black:', black_pix, '%')
+    (thresh, blackAndWhiteImage) = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
 
-outBW = np.zeros_like(blackAndWhiteImage)
-outBW[mask] = blackAndWhiteImage[mask]
+    black_pix = int((np.sum(blackAndWhiteImage[mask] == 0) / np.sum(mask == 1)) * 100)
+    print(str(r), 'black:', black_pix, '%')
 
-outG = np.zeros_like(gray)
-outG[mask] = gray[mask]
+    outBW = np.zeros_like(blackAndWhiteImage)
+    outBW[mask] = blackAndWhiteImage[mask]
 
-#cv2.polylines(img, [np.int32(poly)], True, (255, 0, 0), 3)
+    outG = np.zeros_like(gray)
+    outG[mask] = gray[mask]
 
-cv2.polylines(img, [np.int32(features.CheeksBeard)], True, (255, 0, 0), 3)
+    cv2.polylines(img, [np.int32(poly)], True, (255, 0, 0), 3)
 
-for i in range(68):
-    cv2.circle(img, (landmarks.part(i).x, landmarks.part(i).y), 3, (0, 255, 0), -1)
+    ##for i in range(68):
+    ##    cv2.circle(img, (landmarks.part(i).x, landmarks.part(i).y), 3, (0, 255, 0), -1)
     
-cv2.imshow("Mask1", outG)
-cv2.imshow("Mask2", outBW)  
-cv2.imshow("Face", img)
+##cv2.imshow("Mask1", outG)
+##cv2.imshow("Mask2", outBW)  
+##cv2.imshow("Face", img)
