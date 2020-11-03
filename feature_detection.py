2caa42e85e6d4be4b27452b184d5f84961aac267
edwin-od <67093951+edwin-od@users.noreply.github.com>
2020-11-03 17:00:31 +0200

    Update feature_detection.py

--- feature_detection.py
+++ feature_detection.py
@@ -2,75 +2,325 @@ import cv2
 import numpy as np
 import dlib
 
-from isolated_features import IsolatedFeatures
+from IsolatedFeatures import IsolatedFeatures
+from DecisionTree import Feature as DTFeature
+from DecisionTree import DecisionTable
+from KNN import Iteration
+from KNN import KNNTable
 
-#r = 8
+detector = dlib.get_frontal_face_detector()
+predictor =  dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-for r in range(1, 15):    
-    url = 'input' + str(r) + '.jpg'
-
+def calculateFacePercentage(url, feature, AdaptiveBlockSize, AdaptiveC):
     img = cv2.imread(url)
     img = cv2.resize(img, (320, 400),  interpolation = cv2.INTER_AREA)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    detector = dlib.get_frontal_face_detector()
-    predictor =  dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    faces = detector(gray)
 
-    face = detector(gray)[0]
+    if len(faces) == 0 or len(faces) > 1:
+        return -1
 
+    face = faces[0]
+    
     landmarks = predictor(gray, face)
     features = IsolatedFeatures(landmarks)
 
+    FullPoly = features.FullFace
+
+    LeftPoly = None
+    RightPoly = None
+
+    if feature == 0:
+        LeftPoly = features.LeftCheek
+        RightPoly = features.RightCheek
+    elif feature == 1:
+        LeftPoly = features.LeftUnderEye
+        RightPoly = features.RightUnderEye
+    else:
+        FullPoly = features.FullCheeks
+        LeftPoly = features.LeftLips
+        RightPoly = features.RightLips
+
     #Average Face Brightness
     fullMask = np.zeros((img.shape[0], img.shape[1]))
-    cv2.fillConvexPoly(fullMask, features.FullFace, 1)
+    cv2.fillConvexPoly(fullMask, FullPoly, 1)
     fullMask = fullMask.astype(np.bool)
     AVG_pix_gray = np.sum(gray[fullMask]) / np.sum(fullMask == 1)
     (thresh, blackAndWhiteImage) = cv2.threshold(gray, AVG_pix_gray, 255, cv2.THRESH_BINARY)
 
     LeftMask = np.zeros((img.shape[0], img.shape[1]))
-    cv2.fillConvexPoly(LeftMask, features.LeftCheek, 1)
+    cv2.fillConvexPoly(LeftMask, LeftPoly, 1)
     LeftMask = LeftMask.astype(np.bool)
     LeftBlackPix = int((np.sum(blackAndWhiteImage[LeftMask] == 0) / np.sum(LeftMask == 1)) * 100)
 
     RightMask = np.zeros((img.shape[0], img.shape[1]))
-    cv2.fillConvexPoly(RightMask, features.RightCheek, 1)
+    cv2.fillConvexPoly(RightMask, RightPoly, 1)
     RightMask = RightMask.astype(np.bool)
     RightBlackPix = int((np.sum(blackAndWhiteImage[RightMask] == 0) / np.sum(RightMask == 1)) * 100)
 
     if LeftBlackPix < RightBlackPix:
         mask = LeftMask
-        poly = features.LeftCheek
     else:
         mask = RightMask
-        poly = features.RightCheek
 
-    FaceMask = np.zeros((img.shape[0], img.shape[1]))
-    cv2.fillConvexPoly(FaceMask, features.FullFace, 1)
-    FaceMask = FaceMask.astype(np.bool)
+    blackAndWhiteImage = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                               cv2.THRESH_BINARY, AdaptiveBlockSize, AdaptiveC)
+    return int((np.sum(blackAndWhiteImage[mask] == 0) / np.sum(mask == 1)) * 100)
 
-    #threshold = np.sum(gray[mask]) / np.sum(mask == 1)
-    #threshold = np.mean(gray[mask])
 
-    blackAndWhiteImage = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 3)
+youngCheeksY = 0
+oldCheeksY = 0
+youngCheeksO = 0
+oldCheeksO = 0
+sumCheeks = 0
 
-    black_pix = int((np.sum(blackAndWhiteImage[mask] == 0) / np.sum(mask == 1)) * 100)
-    print('Y', str(r), 'black:', black_pix, '%')
+youngUnderEyeY = 0
+oldUnderEyeY = 0
+youngUnderEyeO = 0
+oldUnderEyeO = 0
+sumUnderEye = 0
 
-    outBW = np.zeros_like(blackAndWhiteImage)
-    outBW[mask] = blackAndWhiteImage[mask]
+youngLipsY = 0
+oldLipsY = 0
+youngLipsO = 0
+oldLipsO = 0
+sumLips = 0
 
-    outG = np.zeros_like(gray)
-    outG[mask] = gray[mask]
+arrYoungCheeks = []
+arrOldCheeks = []
 
-    cv2.polylines(img, [np.int32(poly)], True, (255, 0, 0), 2)
-    cv2.polylines(outBW, [np.int32(poly)], True, (255, 0, 0), 1)
-    cv2.polylines(outG, [np.int32(poly)], True, (255, 0, 0), 1)
+arrYoungUnderEye = []
+arrOldUnderEye = []
 
-    ##for i in range(68):
-    ##    cv2.circle(img, (landmarks.part(i).x, landmarks.part(i).y), 3, (0, 255, 0), -1)
+arrYoungLips = []
+arrOldLips = []
 
-##cv2.imshow("Mask1", outG)
-##cv2.imshow("Mask2", outBW)  
-##cv2.imshow("Face", img)
+scans = 25
+
+YoungScans = int(str(int(scans / 2)))
+YoungOffset = 50
+
+OldScans = int(str(int(scans / 2)))
+OldOffset = 30
+
+CheeksBlockSize = 11
+CheeksC = 4
+UnderEyeBlockSize = 9 #11|7 or 9|7/6/5 -> 9|5 best
+UnderEyeC = 5
+LipsBlockSize = 7 #9|5/6/7 or 7|6 -> 9|6 or 7|5/6/7 -> 7|6 best small set - and 7|7 best big set
+LipsC = 7
+
+for r in range(1, YoungScans + 1):
+    url = 'cropped\\128\\male\\age_20_24\\pic_' + str(r + YoungOffset).zfill(4) + '.png'
+    cheek = calculateFacePercentage(url, 0, CheeksBlockSize, CheeksC)
+    underEye = calculateFacePercentage(url, 1, UnderEyeBlockSize, UnderEyeC)
+    lips = calculateFacePercentage(url, 2, LipsBlockSize, LipsC)
+    if min(cheek, underEye, lips) == -1:
+        YoungScans -= 1
+        print('ERROR: Young Scans decreased from', YoungScans + 1, 'to', YoungScans)
+        continue
+    pr = 'Y'+str(r)+' -> '+'cheeks: '+str(cheek)+'% | under eye: '+str(underEye)+'% | lips: '+str(lips)+'%'
+    print(pr)
+    sumCheeks += cheek
+    sumUnderEye += underEye
+    sumLips += lips
+    arrYoungCheeks.append(cheek)
+    arrYoungUnderEye.append(underEye)
+    arrYoungLips.append(lips)
+    
+for r in range(1, OldScans + 1):
+    url = 'cropped\\128\\male\\age_55_59\\pic_' + str(r + OldOffset).zfill(4) + '.png'
+    cheek = calculateFacePercentage(url, 0, CheeksBlockSize, CheeksC)
+    underEye = calculateFacePercentage(url, 1, UnderEyeBlockSize, UnderEyeC)
+    lips = calculateFacePercentage(url, 2, LipsBlockSize, LipsC)
+    if min(cheek, underEye, lips) == -1:
+        OldScans -= 1
+        print('ERROR: Old Scans decreased from', OldScans + 1, 'to', OldScans)
+        continue
+    pr = 'O'+str(r)+' -> '+'cheeks: '+str(cheek)+'% | under eye: '+str(underEye)+'% | lips: '+str(lips)+'%'
+    print(pr)
+    sumCheeks += cheek
+    sumUnderEye += underEye
+    sumLips += lips
+    arrOldCheeks.append(cheek)
+    arrOldUnderEye.append(underEye)
+    arrOldLips.append(lips)
+
+#Calculating Efficiencies
+
+AverageCheeks = sumCheeks / (YoungScans + OldScans)
+AverageUnderEyes = sumUnderEye / (YoungScans + OldScans)
+AverageLips = sumLips / (YoungScans + OldScans)
+
+for i in range(0, YoungScans):
+    if arrYoungCheeks[i] <= AverageCheeks:
+        youngCheeksY += 1
+    else:
+        oldCheeksY += 1
+    if arrYoungUnderEye[i] <= AverageUnderEyes:
+        youngUnderEyeY += 1
+    else:
+        oldUnderEyeY += 1
+    if arrYoungLips[i] <= AverageLips:
+        youngLipsY += 1
+    else:
+        oldLipsY += 1
+        
+for i in range(0, OldScans):
+    if arrOldCheeks[i] <= AverageCheeks:
+        youngCheeksO += 1
+    else:
+        oldCheeksO += 1
+    if arrOldUnderEye[i] <= AverageUnderEyes:
+        youngUnderEyeO += 1
+    else:
+        oldUnderEyeO += 1
+    if arrOldLips[i] <= AverageLips:
+        youngLipsO += 1
+    else:
+        oldLipsO += 1
+
+CheekYoungEfficiency = ((YoungScans-oldCheeksY)/YoungScans)
+CheekOldEfficiency = ((OldScans-youngCheeksO)/OldScans)
+
+UnderEyeYoungEfficiency = ((YoungScans-oldUnderEyeY)/YoungScans)
+UnderEyeOldEfficiency = ((OldScans-youngUnderEyeO)/OldScans)
+
+LipsYoungEfficiency = ((YoungScans-oldLipsY)/YoungScans)
+LipsOldEfficiency = ((OldScans-youngLipsO)/OldScans)
+
+print('Cheeks Efficiency -> Young',int(CheekYoungEfficiency*100),'% Old', int(CheekOldEfficiency*100),'%')
+
+print('Under Eye Efficiency -> Young',int(UnderEyeYoungEfficiency*100),'% Old', int(UnderEyeOldEfficiency*100),'%')
+
+print('Lips Efficiency -> Young',int(LipsYoungEfficiency*100),'% Old', int(LipsOldEfficiency*100),'%')
+
+#KNN
+
+iterations = []
+
+for i in range(0, YoungScans):
+    iterations.append(Iteration([arrYoungCheeks[i],
+                                arrYoungUnderEye[i],
+                                arrYoungLips[i]],
+                                'young'))
+
+for i in range(0, OldScans):    
+    iterations.append(Iteration([arrOldCheeks[i],
+                                arrOldUnderEye[i],
+                                arrOldLips[i]],
+                                'old'))
+
+table = KNNTable(iterations, ['young', 'old'])
+
+r = 40
+
+url = 'cropped\\128\\male\\age_25_29\\pic_' + str(r).zfill(4) + '.png'
+img1 = cv2.imread(url)
+img1 = cv2.resize(img1, (320, 400),  interpolation = cv2.INTER_AREA)
+cv2.imshow("Face", img1)
+cheek = calculateFacePercentage(url, 0, CheeksBlockSize, CheeksC)
+underEye = calculateFacePercentage(url, 1, UnderEyeBlockSize, UnderEyeC)
+lips = calculateFacePercentage(url, 2, LipsBlockSize, LipsC)
+if min(cheek, underEye, lips) != -1:
+    pr = 'I'+str(r)+' -> '+'cheeks: '+str(cheek)+'% | under eye: '+str(underEye)+'% | lips: '+str(lips)+'%'
+    print(pr)
+##    print(table.process(Iteration([arrOldCheeks[i],
+##                                arrOldUnderEye[i],
+##                                arrOldLips[i]], None), int((YoungScans + OldScans) / 2)))
+    
+    new = Iteration([
+        cheek,
+        underEye,
+        lips],
+            None)
+    
+    k = 7
+    
+    neighbors = table.findNeighbors(new, k)
+    for n in neighbors:
+        s = ''
+        for f in n.featureValues:
+            s += ' ' + str(f)
+        s += ' ' + str(n.target)
+        print(s)
+    print('_________________________')
+    
+    print(table.process(new, k, False))
+else:
+    print('ERROR: Image could not be processed')
+
+
+#Decision Tree
+
+##AverageCheeks = sumCheeks / (YoungScans + OldScans)
+##AverageUnderEyes = sumUnderEye / (YoungScans + OldScans)
+##AverageLips = sumLips / (YoungScans + OldScans)
+##
+##arrCheeks = []
+##arrUnderEye = []
+##arrLips = []
+##
+##arr = []
+##
+##for i in range(0, YoungScans):
+##    arr.append('young')
+##    if arrYoungCheeks[i] <= AverageCheeks:
+##        youngCheeksY += 1
+##        arrCheeks.append('clear')
+##    else:
+##        oldCheeksY += 1
+##        arrCheeks.append('wrinkled')
+##    if arrYoungUnderEye[i] <= AverageUnderEyes:
+##        youngUnderEyeY += 1
+##        arrUnderEye.append('clear')
+##    else:
+##        oldUnderEyeY += 1
+##        arrUnderEye.append('wrinkled')
+##    if arrYoungLips[i] <= AverageLips:
+##        youngLipsY += 1
+##        arrLips.append('clear')
+##    else:
+##        oldLipsY += 1
+##        arrLips.append('wrinkled')
+##        
+##for i in range(0, OldScans):
+##    arr.append('old')
+##    if arrOldCheeks[i] <= AverageCheeks:
+##        youngCheeksO += 1
+##        arrCheeks.append('clear')
+##    else:
+##        oldCheeksO += 1
+##        arrCheeks.append('wrinkled')
+##    if arrOldUnderEye[i] <= AverageUnderEyes:
+##        youngUnderEyeO += 1
+##        arrUnderEye.append('clear')
+##    else:
+##        oldUnderEyeO += 1
+##        arrUnderEye.append('wrinkled')
+##    if arrOldLips[i] <= AverageLips:
+##        youngLipsO += 1
+##        arrLips.append('clear')
+##    else:
+##        oldLipsO += 1
+##        arrLips.append('wrinkled')
+##
+##print('Cheeks Efficiency -> Young',int(((YoungScans-oldCheeksY)/YoungScans)*100),'% Old',
+##       int(((OldScans-youngCheeksO)/OldScans)*100),'%')
+##
+##print('Under Eye Efficiency -> Young',int(((YoungScans-oldUnderEyeY)/YoungScans)*100),'% Old',
+##       int(((OldScans-youngUnderEyeO)/OldScans)*100),'%')
+##
+##print('Lips Efficiency -> Young',int(((YoungScans-oldLipsY)/YoungScans)*100),'% Old',
+##       int(((OldScans-youngLipsO)/OldScans)*100),'%')
+##
+##Age = DTFeature('Age', ['young', 'old'], arr, None)
+##Cheeks = DTFeature('Cheeks', ['clear', 'wrinkled'], arrCheeks, Age)
+##UnderEye = DTFeature('Under Eye', ['clear', 'wrinkled'], arrUnderEye, Age)
+##Lips = DTFeature('Lips', ['clear', 'wrinkled'], arrLips, Age)
+##table = DecisionTable([Cheeks, UnderEye, Lips], Age)
+##tree = table.generateDecisionTree()
+##tree.printTree()
+
